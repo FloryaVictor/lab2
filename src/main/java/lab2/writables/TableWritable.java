0674f3490f49ded1ed422888c172cc07406c91ea
FloryaVictor <lapunixa@yandex.ru>
2020-10-10 23:42:21 +0300

    Scripted auto-commit on change (2020-10-10 23:42:21) by gitwatch.sh

--- src/main/java/lab2/writables/TableWritable.java
+++ src/main/java/lab2/writables/TableWritable.java
@@ -1,4 +1,19 @@
 package lab2.writables;
 
-public class TableWritable {
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class TableWritable implements Writable {
+    @Override
+    public void write(DataOutput out) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+
+    }
 }

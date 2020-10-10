a991d62b5b066193534264fa71edd4007d4697b0
FloryaVictor <lapunixa@yandex.ru>
2020-10-10 22:43:33 +0300

    Scripted auto-commit on change (2020-10-10 22:43:33) by gitwatch.sh

--- src/main/java/lab2/Main.java
+++ src/main/java/lab2/Main.java
@@ -19,7 +19,6 @@ public class Main {
         job.setJobName("Reduce side join");
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         job.setNumReduceTasks(15);

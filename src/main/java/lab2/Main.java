a0b1ec06772da97f0862114530d1dc3f487e53ca
FloryaVictor <lapunixa@yandex.ru>
2020-10-10 22:32:57 +0300

    Scripted auto-commit on change (2020-10-10 22:32:57) by gitwatch.sh

--- src/main/java/lab2/Main.java
+++ src/main/java/lab2/Main.java
@@ -19,6 +19,7 @@ public class Main {
         job.setJobName("Word count");
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
 //        job.setMapperClass(WordMapper.class);
 //        job.setReducerClass(WordReducer.class);
         job.setOutputKeyClass(Text.class);

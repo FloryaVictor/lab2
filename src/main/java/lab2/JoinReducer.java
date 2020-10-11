2f1f46f6219f503835c3847f36faff4846caf784
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 02:33:51 +0300

    Scripted auto-commit on change (2020-10-12 02:33:51) by gitwatch.sh

--- src/main/java/lab2/JoinReducer.java
+++ src/main/java/lab2/JoinReducer.java
@@ -10,6 +10,15 @@ public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
     @Override
     protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
-        
+        String code = iter.next().toString();
+        if (iter.hasNext()){
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            int count = 0, sum = 0;
+            while (iter.hasNext())
+            {
+                
+            }
+        }
     }
 }

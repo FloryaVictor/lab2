d49cdb40de5ae8c3b610faf0e0b7b11e2bec377a
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 02:08:56 +0300

    Scripted auto-commit on change (2020-10-12 02:08:56) by gitwatch.sh

--- src/main/java/lab2/Main.java
+++ src/main/java/lab2/Main.java
@@ -21,7 +21,7 @@ public class Main {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TableMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, TableMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-//        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
 //        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
 //        job.setReducerClass(JoinReducer.class);
         job.setMapOutputKeyClass(TextPair.class);

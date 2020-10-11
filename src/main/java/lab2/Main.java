b19ea13f40198506101ae223d51370a520dcd700
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 01:56:02 +0300

    Scripted auto-commit on change (2020-10-12 01:56:01) by gitwatch.sh

--- src/main/java/lab2/Main.java
+++ src/main/java/lab2/Main.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import java.lang.*;
 
@@ -16,8 +17,8 @@ public class Main {
         Job job = Job.getInstance();
         job.setJarByClass(Main.class);
         job.setJobName("Reduce side join");
-//        MultipleInputs.addInputPath(job, new Path(args[0]),);
-//        MultipleInputs.addInputPath(job, new Path(args[1]),);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, Table );
+        MultipleInputs.addInputPath(job, new Path(args[1]),);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);

3957e4b1148ccce1f64cc0aaf9adb329028df01f
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 02:19:18 +0300

    Scripted auto-commit on change (2020-10-12 02:19:18) by gitwatch.sh

--- src/main/java/lab2/TextPair.java
+++ src/main/java/lab2/TextPair.java
@@ -65,6 +65,16 @@ public class TextPair implements WritableComparable<TextPair> {
         public int compare(TextPair o1, TextPair o2) {
             return o1.first.compareTo(o2.first);
         }
+
+        @Override
+        public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+            return 0;
+        }
+
+        @Override
+        public int compare(Object o1, Object o2) {
+            return 0;
+        }
     }
 
 }

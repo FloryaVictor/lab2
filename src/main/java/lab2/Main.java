57a2029a50aee4340c49e9483311201b50ca06d1
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 18:11:57 +0300

    Scripted auto-commit on change (2020-10-12 18:11:57) by gitwatch.sh

--- src/main/java/lab2/Main.java
+++ src/main/java/lab2/Main.java
@@ -29,7 +29,7 @@ public class Main {
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        job.setNumReduceTasks(2);
+        job.setNumReduceTasks(0);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }

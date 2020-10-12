21f482a647f33159cdfb62fb14279d8df3caae8e
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 20:05:07 +0300

    Scripted auto-commit on change (2020-10-12 20:05:07) by gitwatch.sh

--- src/main/java/lab2/FirstComparator.java
+++ src/main/java/lab2/FirstComparator.java
@@ -16,12 +16,12 @@ import org.apache.hadoop.io.WritableComparator;
 //}
 
 public class FirstComparator extends WritableComparator{
-    protected {
+    protected FirstComparator(){
         super(KeyPair.class, true);
     }
 
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
-        return super.compare(a, b);
+        return ((KeyPair)a).id.get() - ((KeyPair)b).id.get();
     }
 }

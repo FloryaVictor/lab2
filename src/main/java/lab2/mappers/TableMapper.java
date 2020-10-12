a5a8478bec8272cb2efbd0bd610871ecc8f23e2a
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 19:39:40 +0300

    Scripted auto-commit on change (2020-10-12 19:39:40) by gitwatch.sh

--- src/main/java/lab2/mappers/TableMapper.java
+++ src/main/java/lab2/mappers/TableMapper.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class TableMapper extends Mapper<LongWritable, Text, KeyPair, Text> {
     public static final int ID = 14;
     public static final int DELAY = 18;
-    public static final int  IS_CANCELLED = 0;
+    public static final int  IS_CANCELLED = 19;
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() == 0){

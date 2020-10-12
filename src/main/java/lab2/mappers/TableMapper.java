1349a039d1c16be0917190a4c6c2100f7c598993
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 15:52:17 +0300

    Scripted auto-commit on change (2020-10-12 15:52:17) by gitwatch.sh

--- src/main/java/lab2/mappers/TableMapper.java
+++ src/main/java/lab2/mappers/TableMapper.java
@@ -11,7 +11,12 @@ public class TableMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] info  = value.toString().split(",");
-        if (Integer.parseInt(info[18]) > 0)
-            context.write(new TextPair(info[14], "1"), new Text(info[18]));
+        try {
+            if (Integer.parseInt(info[18]) > 0)
+                context.write(new TextPair(info[14], "1"), new Text(info[18]));
+        }catch (Exception e)
+        {
+            System.out.println(value);
+        }
     }
 }

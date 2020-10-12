22f2c9cee632b7428de631aabfe30737fc04e0ce
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 19:38:14 +0300

    Scripted auto-commit on change (2020-10-12 19:38:14) by gitwatch.sh

--- src/main/java/lab2/mappers/TableMapper.java
+++ src/main/java/lab2/mappers/TableMapper.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class TableMapper extends Mapper<LongWritable, Text, KeyPair, Text> {
-    public static final 
+    public static final id
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() == 0){

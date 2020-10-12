9e7df8d6b40202fb585b8fa1b4bbd5730e136fa5
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 20:09:10 +0300

    Scripted auto-commit on change (2020-10-12 20:09:10) by gitwatch.sh

--- src/main/java/lab2/FirstComparator.java
+++ src/main/java/lab2/FirstComparator.java
@@ -4,24 +4,24 @@ import org.apache.hadoop.io.RawComparator;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
-//public class FirstComparator implements RawComparator<KeyPair> {
-//    public int compare(KeyPair o1, KeyPair o2) {
-//        return o1.id.compareTo(o2.id);
-//    }
-//
-//    @Override
-//    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-//        return 0;
-//    }
-//}
-
-public class FirstComparator extends WritableComparator{
-    protected FirstComparator(){
-        super(KeyPair.class, true);
+public class FirstComparator implements RawComparator<KeyPair> {
+    public int compare(KeyPair o1, KeyPair o2) {
+        return o1.id.compareTo(o2.id);
     }
 
     @Override
-    public int compare(WritableComparable a, WritableComparable b) {
-        return ((KeyPair)a).id.get() - ((KeyPair)b).id.get();
+    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+        return 0;
     }
 }
+
+//public class FirstComparator extends WritableComparator{
+//    protected FirstComparator(){
+//        super(KeyPair.class, true);
+//    }
+//
+//    @Override
+//    public int compare(WritableComparable a, WritableComparable b) {
+//        return ((KeyPair)a).id.get() - ((KeyPair)b).id.get();
+//    }
+//}

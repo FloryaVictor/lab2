4454331c8fdfab40e9bd13c07ce2e04312d1b361
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 17:23:35 +0300

    Scripted auto-commit on change (2020-10-12 17:23:35) by gitwatch.sh

--- src/main/java/lab2/JoinReducer.java
+++ src/main/java/lab2/JoinReducer.java
@@ -9,7 +9,6 @@ import java.util.Iterator;
 public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
     @Override
     protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        System.out.println(key.first + " " + "HLADLPSDLPSDPLSDPLSSPDL");
         Iterator<Text> iter = values.iterator();
         Text airport = iter.next();
         if (iter.hasNext()){

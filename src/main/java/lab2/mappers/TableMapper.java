3d1d93a0083cce49ddf3fd15565f126c70393215
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 16:42:13 +0300

    Scripted auto-commit on change (2020-10-12 16:42:13) by gitwatch.sh

--- src/main/java/lab2/mappers/TableMapper.java
+++ src/main/java/lab2/mappers/TableMapper.java
@@ -14,7 +14,7 @@ public class TableMapper extends Mapper<LongWritable, Text, TextPair, Text> {
         try {
             if (Float.parseFloat(info[18]) > 0)
                 context.write(new TextPair(info[14], "1"), new Text(info[18]));
-            System.out.println(Integer.parseInt(info[15]));
+            System.out.println(Integer.parseInt(info[14]));
         }catch (Exception e)
         {
 

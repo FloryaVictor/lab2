31d13e1e199767b9223ba3a2d98233441dab04de
FloryaVictor <lapunixa@yandex.ru>
2020-10-12 17:07:31 +0300

    Scripted auto-commit on change (2020-10-12 17:07:31) by gitwatch.sh

--- src/main/java/lab2/mappers/TableMapper.java
+++ src/main/java/lab2/mappers/TableMapper.java
@@ -13,10 +13,11 @@ public class TableMapper extends Mapper<LongWritable, Text, TextPair, Text> {
         if (value.toString().startsWith("\"YEAR\",\"QUARTER\",\"MONTH\",\"DAY_OF_MONTH\",\"DAY_OF_WEEK\",\"FL_DATE\",\"UNIQUE_CARRIER\",\"AIRLINE_ID\",\"CARRIER\",\"TAIL_NUM\",\"FL_NUM\",\"ORIGIN_AIRPORT_ID\",\"ORIGIN_AIRPORT_SEQ_ID\",\"ORIGIN_CITY_MARKET_ID\",\"DEST_AIRPORT_ID\",\"WHEELS_ON\",\"ARR_TIME\",\"ARR_DELAY\",\"ARR_DELAY_NEW\",\"CANCELLED\",\"CANCELLATION_CODE\",\"AIR_TIME\",\"DISTANCE\",")){
             return;
         }
-        String[] info  = value.toString().split(",");
+        String[] info = value.toString().replaceAll("\"","").split(",");
         try {
             if (Float.parseFloat(info[18]) > 0)
                 context.write(new TextPair(info[14], "1"), new Text(info[18]));
+            System.out.println(info[14]);
         }catch (Exception e) {
             if (e.getClass() == IOException.class || e.getClass() == InterruptedException.class){
                 throw e;
